9d750908f8aa35495d9bc1bf0542681f0bffcdf6
DarrenN <info@v25media.com>
2015-02-02 19:06:24 -0500

    filter nils

--- src/calligraphus/core.clj
+++ src/calligraphus/core.clj
@@ -18,7 +18,7 @@
 (def chapters (yaml/parse-string (slurp chapter-yaml)))
 
 ; List of url formatted chapter names - used to hit API for group info
-(def chapter-urls (map :meetup_url chapters))
+(def chapter-urls (remove nil? (map :meetup_url chapters)))
 
 ; Groups endpoints
 (def group-uri-template "http://api.meetup.com/2/groups?radius=25.0&order=id&group_urlname=%s&desc=false&offset=0&photo-host=public&format=json&page=50&key=%s")
@@ -80,17 +80,3 @@
         p (get-photos ge photo-chan)
         photo-result (async/<!! photo-chan)]
     (swap! chapter-results conj photo-result)))
-
-;; ; Seq through the chapter urls and get their representations from API
-;; (def group-data
-;;   (let [c (async/chan)]
-;;     ;; get em
-;;     (doseq [group chapter-urls]
-;;       (async-get (format chapter-uri-template group api-key) c))
-;;     ;; gather em
-;;     (doseq [_ chapter-urls]
-;;       (let [r (async/<!! c)
-;;             body (walk/keywordize-keys (parse-string (:body r)))
-;;             results (first (:results body))]
-;;         (swap! chapter-results assoc (:id results) results)))
-;;     @chapter-results))
