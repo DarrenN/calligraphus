a231e6e697585a4cf4adead2ace1b3b200fbcaaf
DarrenN <info@v25media.com>
2015-03-22 13:40:39 -0400

    tmp hardcode

--- src/calligraphus/core.clj
+++ src/calligraphus/core.clj
@@ -9,10 +9,17 @@
             [cheshire.core :refer :all]))
 
 ; Load chapter data
-(def chapter-yaml (io/file "resources/yaml/chapters.yml"))
+(def in "/Users/shibuya/github/papers-we-love.github.io/source/chapters.yml")
+(def out "/Users/shibuya/github/papers-we-love.github.io/data/meetup/chapters.yml")
+(def chapter-yaml (io/file in))
 (def chapters (yaml/parse-string (slurp chapter-yaml)))
 
 ; List of url formatted chapter names - used to hit API for group info
 (def chapter-urls (remove nil? (map :meetup_url chapters)))
 
 ;; (spit "/tmp/meetup0.json" (generate-string final-map))
+;; /Users/shibuya/github/papers-we-love.github.io/source/chapters.yml
+
+
+
+;; (spit out (yaml/generate-string (meetup/get-chapters chapter-urls)))
