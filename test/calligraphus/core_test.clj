41d1ec02d40285e21392fdd8200a31351f531b50
DarrenN <info@v25media.com>
2015-03-22 18:17:34 -0400

    Unit tests for core.clj

--- test/calligraphus/core_test.clj
+++ test/calligraphus/core_test.clj
@@ -1,7 +1,43 @@
 (ns calligraphus.core-test
   (:require [clojure.test :refer :all]
-            [calligraphus.core :refer :all]))
+            [calligraphus.core :refer :all]
+            [calligraphus.meetup :as meetup]
+            [calligraphus.facebook :as facebook]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def meetup-urls '({:api "meetup", :url "http://meetup.com/foo"}
+                   {:api "meetup", :url "http://meetup.com/bar"}
+                   {:api "meetup", :url "http://meetup.com/baz"}))
+
+(def facebook-urls '({:api "facebook", :url "http://facebook.com/foo"}
+                     {:api "facebook", :url "http://facebook.com/bar"}
+                     {:api "facebook", :url "http://facebook.com/baz"}))
+
+(def busted-urls '({:api "", :url "http://twiiter.com/foo"}))
+
+(deftest test-process-url
+  (testing "process-url returns maps"
+    (is (= (process-url "foo") {:api "meetup", :url "foo"})
+        "process-url returns a meetup map if not a real url")
+
+    (is (= (process-url "http://facebook.com/foo")
+           {:api "facebook", :url "http://facebook.com/foo"})
+        "process-url returns a facebook map")
+
+    (is (= (process-url "http://twitter.com/foo")
+           {:api "", :url "http://twitter.com/foo"})
+        "process-url returns empty :api if not matched")))
+
+(deftest test-api-dispatch
+  (testing "api-dispatch calls different ns/functions depending on :api"
+    (let [mchapters (atom '())
+          fchapters (atom '())]
+      (with-redefs [meetup/get-chapters (fn [chaps] (reset! mchapters chaps))
+                    facebook/get-chapters (fn [chaps] (reset! fchapters chaps))]
+        (let [resp1 (api-dispatch meetup-urls)
+              resp2 (api-dispatch facebook-urls)]
+          (is (= @mchapters meetup-urls)
+              "Meetup chapters sent to meetup/get-chapters")
+          (is (= @fchapters facebook-urls)
+              "Facebook chapters sent to facebook/get-chapters")
+          (is (thrown? IllegalArgumentException (api-dispatch busted-urls))
+              "Busted chapters throw and exception in the multimethod"))))))
